87d4480c0f0afcfdf54af2530da9c8dde903ea33
Sifeddine <sifeddinebaouz@gmail.com>
2024-12-09 12:49:55 -0800

    Initial commit

---
+++ cabinet-medical-api/src/main/java/com/baouz/cabinet/CabinetMedicalApiApplication.java
@@ -0,0 +1,13 @@
+package com.baouz.cabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetMedicalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetMedicalApiApplication.class, args);
+	}
+
+}
